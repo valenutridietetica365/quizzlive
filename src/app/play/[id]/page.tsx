6254b9edf05b5399ac788c609cce0e72ddfbf842
josen <josen@example.com>
2026-03-13 22:09:41 -0300

    fix: move chaos store to right-top to avoid ranking overlap

--- src/app/play/[id]/page.tsx
+++ src/app/play/[id]/page.tsx
@@ -172,7 +172,7 @@ export default function StudentPlay() {
                                 </div>
                             )}
                             
-                            <div className="fixed left-4 top-4 z-[60]">
+                            <div className="fixed right-4 top-20 z-[60]">
                                 <ChaosStore
                                     coins={myCoins}
                                     hasShield={hasShield}
